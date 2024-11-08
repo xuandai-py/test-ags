1dc081d528ace7182a4f546560ea696d060b7910
Bui Nguyen Xuan Dai <dai.bui@ags.com.vn>
2024-11-08 11:26:06 +0700

    update route

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import inject from "@rollup/plugin-inject";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      mkcert(),
+      inject({
+        // => that should be first under plugins array
+        $: "jquery",
+        jQuery: "jquery",
+        moment: "moment",
+      }),
+      react(),
+    ],
+
+    build: {
+      outDir: "C:/Sites/New_AGS_Site/dist",
+      emptyOutDir: true,
+    },
+
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+
+    server: {
+      port: env.VITE_AGS_PORT,
+      proxy: {
+        "/api": {
+          target: env.VITE_AGS_API_URL,
+          changeOrigin: true,
+          secure: !(mode === "development"),
+        },
+      },
+      cors: false,
+    },
+
+    base: "/",
+    optimizeDeps: {
+      include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
+    },
+  };
+});
